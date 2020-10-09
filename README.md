a04ae899501f955f5ed148cf525732f089be60f0
Matthew Elphick <matthew.elphick@threadsstyling.com>
2020-10-09 19:31:23 +0100

    fix: 🐛 Run build as prepackage, add debug logging

--- README.md
+++ README.md
@@ -61,3 +61,9 @@ See the [actions tab](https://github.com/actions/typescript-action/actions) for 
 ## Usage:
 
 After testing you can [create a v1 tag](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md) to reference the stable and latest V1 action
+
+## Example:
+
+<!-- TEMPLATE_LIST_START -->
+List Here
+<!-- TEMPLATE_LIST_END -->
--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,116 @@
+import {promises as fs} from 'fs'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
+interface Response {
+  organization: {
+    repositories: {
+      pageInfo: {
+        hasNextPage: boolean
+        endCursor?: string
+      }
+      nodes: Item[]
+    }
+  }
+}
+interface Item {
+  name: string
+  nameWithOwner: string
+  url: string
+  templateRepository: null | {
+    name: string
+    owner: {
+      login: string
+    }
+  }
+}
+
 async function run(): Promise<void> {
   try {
-    const token: string = core.getInput('token')
-    const octokit = github.getOctokit(token)
-    const {repo} = github.context
-    const org = repo.owner
-
-    const {data} = await octokit.repos.listForOrg({
-      org
+    const readmePath = core.getInput('readmePath') || 'README.md'
+    const readmeContent = await fs.readFile(readmePath, {
+      encoding: 'utf-8'
     })
 
-    const reposProducedByThis = data
+    const token: string = core.getInput('token')
+    const octokit = github.getOctokit(token, {
+      previews: ['baptiste']
+    })
+    const {repo} = github.context
+    const org = core.getInput('org') || repo.owner
+    const repoName = core.getInput('repo') || repo.repo
+
+    let items: Item[] = []
+    let nextPageCursor: string | null | undefined = null
+
+    do {
+      const result: Response = await octokit.graphql(
+        `
+        query orgRepos($owner: String!, $afterCursor: String) {
+          organization(login: $owner) {
+            repositories(first: 100, after:$afterCursor, orderBy:{field:PUSHED_AT, direction:DESC}) {
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+              nodes {
+                name
+                nameWithOwner
+                url
+                templateRepository {
+                  name
+                  owner {
+                    login
+                  }
+                }
+              }
+            }
+          }
+        }
+      `,
+        {
+          owner: org,
+          afterCursor: nextPageCursor
+        }
+      )
+      nextPageCursor = result.organization.repositories.pageInfo.hasNextPage
+        ? result.organization.repositories.pageInfo.endCursor
+        : undefined
+
+      items = items.concat(result.organization.repositories.nodes)
+    } while (nextPageCursor !== undefined)
+
+    core.info(`${items.length} items`)
+
+    const reposProducedByThis = items
       .filter(
         d =>
-          d.template_repository.name === repo.repo &&
-          d.template_repository.owner === org
+          d.templateRepository &&
+          d.templateRepository.name === repoName &&
+          d.templateRepository.owner.login === org
       )
-      .map(d => `[${d.html_url}](${d.full_name})`)
-      .join('\n* ')
+      .map(d => `[${d.url}](${d.nameWithOwner})`)
 
-    const output = `# Repositories using this template\n\n${
-      reposProducedByThis.length ? '' : `* ${reposProducedByThis}`
+    const output = `# ${reposProducedByThis.length} Repositories using ${
+      repoName === repo.repo ? '' : `${repoName} `
+    }template\n\n${
+      reposProducedByThis.length ? `* ${reposProducedByThis.join('\n* ')}` : ''
     }`
 
-    core.info(output)
+    core.info(
+      readmeContent.replace(
+        /<!-- TEMPLATE_LIST_START -->[\s\S]+<!-- TEMPLATE_LIST_END -->/,
+        `<!-- TEMPLATE_LIST_START -->\n${output}\n<!-- TEMPLATE_LIST_END -->`
+      )
+    )
+
+    await fs.writeFile(
+      readmePath,
+      readmeContent.replace(
+        /<!-- TEMPLATE_LIST_START -->[\s\S]+<!-- TEMPLATE_LIST_END -->/,
+        `<!-- TEMPLATE_LIST_START -->\n${output}\n<!-- TEMPLATE_LIST_END -->`
+      )
+    )
   } catch (error) {
     core.setFailed(error.message)
   }
